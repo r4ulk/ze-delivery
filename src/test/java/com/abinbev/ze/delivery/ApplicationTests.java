195b493dd1ce46047726ff84c6233babd125f8f2
Klumpp, Raul <raul.klumpp@wirecard.com>
2020-06-26 21:55:35 -0300

    first commit adding project skeleton

---
+++ src/test/java/com/abinbev/ze/delivery/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abinbev.ze.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
